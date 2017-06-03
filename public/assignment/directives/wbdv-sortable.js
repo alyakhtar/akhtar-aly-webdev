c1f610799a962784285675c36a877295f3090379
Aly Akhtar <Aly@Alys-MacBook-Pro.local>
2017-06-03 13:42:52 -0400

    new home page created and draggable directive started
    Signed-off-by: Aly Akhtar <Aly@Alys-MacBook-Pro.local>

---
+++ public/assignment/directives/wbdv-sortable.js
@@ -0,0 +1,15 @@
+(function(){
+	angular
+		.module('WebAppMaker')
+		.directive('wbdvSortable', wbdvSortable)
+
+		function wbdvSortable(){
+			function linkFunction(scope, element){
+				$(element).sortable();
+			}
+
+			return{
+				link: linkFunction
+			}
+		}
+})();
